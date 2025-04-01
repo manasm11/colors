825e4872403e2bfeeeac39b32baf77fd27692af8
Manas Mishra <manas.m22@gmail.com>
2025-04-01 10:12:11 +0530

    add colors list

---
+++ colors.go
@@ -0,0 +1,510 @@
+package colors
+
+// Colors represents a comprehensive list of named colors
+// with their RGB hex values
+// This can be used for consistent color references across your Go applications
+
+// Basic Colors
+const (
+	Black   = "#000000"
+	White   = "#FFFFFF"
+	Red     = "#FF0000"
+	Green   = "#00FF00"
+	Blue    = "#0000FF"
+	Yellow  = "#FFFF00"
+	Cyan    = "#00FFFF"
+	Magenta = "#FF00FF"
+)
+
+// Gray Shades
+const (
+	Silver     = "#C0C0C0"
+	Gray       = "#808080"
+	DarkGray   = "#A9A9A9"
+	LightGray  = "#D3D3D3"
+	SlateGray  = "#708090"
+	DimGray    = "#696969"
+	Gainsboro  = "#DCDCDC"
+	WhiteSmoke = "#F5F5F5"
+)
+
+// Red Shades
+const (
+	DarkRed     = "#8B0000"
+	Crimson     = "#DC143C"
+	Maroon      = "#800000"
+	IndianRed   = "#CD5C5C"
+	FireBrick   = "#B22222"
+	LightCoral  = "#F08080"
+	Salmon      = "#FA8072"
+	DarkSalmon  = "#E9967A"
+	LightSalmon = "#FFA07A"
+	Tomato      = "#FF6347"
+	OrangeRed   = "#FF4500"
+	DarkOrange  = "#FF8C00"
+	Coral       = "#FF7F50"
+)
+
+// Pink Shades
+const (
+	Pink            = "#FFC0CB"
+	LightPink       = "#FFB6C1"
+	HotPink         = "#FF69B4"
+	DeepPink        = "#FF1493"
+	MediumVioletRed = "#C71585"
+	PaleVioletRed   = "#DB7093"
+)
+
+// Orange and Brown Shades
+const (
+	Orange         = "#FFA500"
+	SandyBrown     = "#F4A460"
+	Peru           = "#CD853F"
+	Chocolate      = "#D2691E"
+	SaddleBrown    = "#8B4513"
+	Sienna         = "#A0522D"
+	Brown          = "#A52A2A"
+	RosyBrown      = "#BC8F8F"
+	Tan            = "#D2B48C"
+	BurlyWood      = "#DEB887"
+	Wheat          = "#F5DEB3"
+	NavajoWhite    = "#FFDEAD"
+	Bisque         = "#FFE4C4"
+	BlanchedAlmond = "#FFEBCD"
+	Cornsilk       = "#FFF8DC"
+)
+
+// Yellow Shades
+const (
+	Gold                 = "#FFD700"
+	Khaki                = "#F0E68C"
+	DarkKhaki            = "#BDB76B"
+	LemonChiffon         = "#FFFACD"
+	PaleGoldenrod        = "#EEE8AA"
+	DarkGoldenrod        = "#B8860B"
+	Goldenrod            = "#DAA520"
+	LightGoldenrodYellow = "#FAFAD2"
+	LightYellow          = "#FFFFE0"
+	PapayaWhip           = "#FFEFD5"
+	Moccasin             = "#FFE4B5"
+	PeachPuff            = "#FFDAB9"
+)
+
+// Green Shades
+const (
+	DarkGreen         = "#006400"
+	ForestGreen       = "#228B22"
+	SeaGreen          = "#2E8B57"
+	MediumSeaGreen    = "#3CB371"
+	LimeGreen         = "#32CD32"
+	Lime              = "#00FF00"
+	SpringGreen       = "#00FF7F"
+	MediumSpringGreen = "#00FA9A"
+	PaleGreen         = "#98FB98"
+	LightGreen        = "#90EE90"
+	DarkSeaGreen      = "#8FBC8F"
+	YellowGreen       = "#9ACD32"
+	OliveDrab         = "#6B8E23"
+	Olive             = "#808000"
+	DarkOliveGreen    = "#556B2F"
+	GreenYellow       = "#ADFF2F"
+	Chartreuse        = "#7FFF00"
+	LawnGreen         = "#7CFC00"
+)
+
+// Cyan/Teal Shades
+const (
+	Teal             = "#008080"
+	DarkCyan         = "#008B8B"
+	LightSeaGreen    = "#20B2AA"
+	CadetBlue        = "#5F9EA0"
+	DarkTurquoise    = "#00CED1"
+	MediumTurquoise  = "#48D1CC"
+	Turquoise        = "#40E0D0"
+	Aqua             = "#00FFFF"
+	Aquamarine       = "#7FFFD4"
+	MediumAquamarine = "#66CDAA"
+	PaleTurquoise    = "#AFEEEE"
+	LightCyan        = "#E0FFFF"
+)
+
+// Blue Shades
+const (
+	Navy           = "#000080"
+	DarkBlue       = "#00008B"
+	MediumBlue     = "#0000CD"
+	MidnightBlue   = "#191970"
+	RoyalBlue      = "#4169E1"
+	SteelBlue      = "#4682B4"
+	DodgerBlue     = "#1E90FF"
+	DeepSkyBlue    = "#00BFFF"
+	CornflowerBlue = "#6495ED"
+	SkyBlue        = "#87CEEB"
+	LightSkyBlue   = "#87CEFA"
+	LightBlue      = "#ADD8E6"
+	PowderBlue     = "#B0E0E6"
+	LightSteelBlue = "#B0C4DE"
+)
+
+// Purple/Violet Shades
+const (
+	Indigo          = "#4B0082"
+	Purple          = "#800080"
+	DarkMagenta     = "#8B008B"
+	DarkViolet      = "#9400D3"
+	DarkSlateBlue   = "#483D8B"
+	BlueViolet      = "#8A2BE2"
+	DarkOrchid      = "#9932CC"
+	Lavender        = "#E6E6FA"
+	MediumPurple    = "#9370DB"
+	MediumOrchid    = "#BA55D3"
+	Thistle         = "#D8BFD8"
+	Plum            = "#DDA0DD"
+	Violet          = "#EE82EE"
+	Orchid          = "#DA70D6"
+	MediumSlateBlue = "#7B68EE"
+	SlateBlue       = "#6A5ACD"
+	RebeccaPurple   = "#663399"
+)
+
+// White/Beige Shades
+const (
+	Snow          = "#FFFAFA"
+	Honeydew      = "#F0FFF0"
+	MintCream     = "#F5FFFA"
+	Azure         = "#F0FFFF"
+	AliceBlue     = "#F0F8FF"
+	GhostWhite    = "#F8F8FF"
+	FloralWhite   = "#FFFAF0"
+	OldLace       = "#FDF5E6"
+	Linen         = "#FAF0E6"
+	AntiqueWhite  = "#FAEBD7"
+	Beige         = "#F5F5DC"
+	Ivory         = "#FFFFF0"
+	Seashell      = "#FFF5EE"
+	LavenderBlush = "#FFF0F5"
+	MistyRose     = "#FFE4E1"
+)
+
+// Material Design Colors - Primary
+const (
+	MaterialRed500        = "#F44336"
+	MaterialPink500       = "#E91E63"
+	MaterialPurple500     = "#9C27B0"
+	MaterialDeepPurple500 = "#673AB7"
+	MaterialIndigo500     = "#3F51B5"
+	MaterialBlue500       = "#2196F3"
+	MaterialLightBlue500  = "#03A9F4"
+	MaterialCyan500       = "#00BCD4"
+	MaterialTeal500       = "#009688"
+	MaterialGreen500      = "#4CAF50"
+	MaterialLightGreen500 = "#8BC34A"
+	MaterialLime500       = "#CDDC39"
+	MaterialYellow500     = "#FFEB3B"
+	MaterialAmber500      = "#FFC107"
+	MaterialOrange500     = "#FF9800"
+	MaterialDeepOrange500 = "#FF5722"
+	MaterialBrown500      = "#795548"
+	MaterialGrey500       = "#9E9E9E"
+	MaterialBlueGrey500   = "#607D8B"
+)
+
+// Flat UI Colors
+const (
+	FlatTurquoise    = "#1ABC9C"
+	FlatEmerald      = "#2ECC71"
+	FlatPeterRiver   = "#3498DB"
+	FlatAmethyst     = "#9B59B6"
+	FlatWetAsphalt   = "#34495E"
+	FlatGreenSea     = "#16A085"
+	FlatNephritis    = "#27AE60"
+	FlatBelizeHole   = "#2980B9"
+	FlatWisteria     = "#8E44AD"
+	FlatMidnightBlue = "#2C3E50"
+	FlatSunFlower    = "#F1C40F"
+	FlatCarrot       = "#E67E22"
+	FlatAlizarin     = "#E74C3C"
+	FlatClouds       = "#ECF0F1"
+	FlatConcrete     = "#95A5A6"
+	FlatOrange       = "#F39C12"
+	FlatPumpkin      = "#D35400"
+	FlatPomegranate  = "#C0392B"
+	FlatSilver       = "#BDC3C7"
+	FlatAsbestos     = "#7F8C8D"
+)
+
+// Social Media Brand Colors
+const (
+	FacebookBlue    = "#3B5998"
+	TwitterBlue     = "#1DA1F2"
+	LinkedInBlue    = "#0077B5"
+	YouTubeRed      = "#FF0000"
+	InstagramPurple = "#C13584"
+	PinterestRed    = "#BD081C"
+	TumblrBlue      = "#35465C"
+	SlackPurple     = "#4A154B"
+	WhatsAppGreen   = "#25D366"
+	TelegramBlue    = "#0088CC"
+	SnapchatYellow  = "#FFFC00"
+	RedditOrange    = "#FF4500"
+	TwitchPurple    = "#9146FF"
+	VimeoBlue       = "#1AB7EA"
+)
+
+// Pantone Colors of the Year (2000-2023)
+const (
+	Pantone2023VivaMagenta    = "#BB2649"
+	Pantone2022VeryPeri       = "#6667AB"
+	Pantone2021Ultimate       = "#939597"
+	Pantone2021Illuminating   = "#F5DF4D"
+	Pantone2020ClassicBlue    = "#0F4C81"
+	Pantone2019LivingCoral    = "#FF6F61"
+	Pantone2018UltraViolet    = "#5F4B8B"
+	Pantone2017Greenery       = "#88B04B"
+	Pantone2016RoseQuartz     = "#F7CAC9"
+	Pantone2016Serenity       = "#92A8D1"
+	Pantone2015Marsala        = "#955251"
+	Pantone2014RadiantOrchid  = "#B163A3"
+	Pantone2013Emerald        = "#009473"
+	Pantone2012TangerineTango = "#DD4124"
+	Pantone2011Honeysuckle    = "#D94F70"
+	Pantone2010Turquoise      = "#45B5AA"
+	Pantone2009Mimosa         = "#F0C05A"
+	Pantone2008BlueIris       = "#5A5B9F"
+	Pantone2007ChiliPepper    = "#9B1B30"
+	Pantone2006SandDollar     = "#DECDBE"
+	Pantone2005BlueTurquoise  = "#53B0AE"
+	Pantone2004TigerLily      = "#E2583E"
+	Pantone2003Aqua           = "#7BC4C4"
+	Pantone2002TrueRed        = "#BF1932"
+	Pantone2001FuchsiaRose    = "#C74375"
+	Pantone2000Cerulean       = "#98B2D1"
+)
+
+// GetNamedColor returns the hex code for a named color
+// If the color name is not found, it returns an empty string
+func GetNamedColor(name string) string {
+	// This is a map of all color names to their hex values
+	colors := map[string]string{
+		"Black":                     Black,
+		"White":                     White,
+		"Red":                       Red,
+		"Green":                     Green,
+		"Blue":                      Blue,
+		"Yellow":                    Yellow,
+		"Cyan":                      Cyan,
+		"Magenta":                   Magenta,
+		"Silver":                    Silver,
+		"Gray":                      Gray,
+		"DarkGray":                  DarkGray,
+		"LightGray":                 LightGray,
+		"SlateGray":                 SlateGray,
+		"DimGray":                   DimGray,
+		"Gainsboro":                 Gainsboro,
+		"WhiteSmoke":                WhiteSmoke,
+		"DarkRed":                   DarkRed,
+		"Crimson":                   Crimson,
+		"Maroon":                    Maroon,
+		"IndianRed":                 IndianRed,
+		"FireBrick":                 FireBrick,
+		"LightCoral":                LightCoral,
+		"Salmon":                    Salmon,
+		"DarkSalmon":                DarkSalmon,
+		"LightSalmon":               LightSalmon,
+		"Tomato":                    Tomato,
+		"OrangeRed":                 OrangeRed,
+		"DarkOrange":                DarkOrange,
+		"Coral":                     Coral,
+		"Pink":                      Pink,
+		"LightPink":                 LightPink,
+		"HotPink":                   HotPink,
+		"DeepPink":                  DeepPink,
+		"MediumVioletRed":           MediumVioletRed,
+		"PaleVioletRed":             PaleVioletRed,
+		"Orange":                    Orange,
+		"SandyBrown":                SandyBrown,
+		"Peru":                      Peru,
+		"Chocolate":                 Chocolate,
+		"SaddleBrown":               SaddleBrown,
+		"Sienna":                    Sienna,
+		"Brown":                     Brown,
+		"RosyBrown":                 RosyBrown,
+		"Tan":                       Tan,
+		"BurlyWood":                 BurlyWood,
+		"Wheat":                     Wheat,
+		"NavajoWhite":               NavajoWhite,
+		"Bisque":                    Bisque,
+		"BlanchedAlmond":            BlanchedAlmond,
+		"Cornsilk":                  Cornsilk,
+		"Gold":                      Gold,
+		"Khaki":                     Khaki,
+		"DarkKhaki":                 DarkKhaki,
+		"LemonChiffon":              LemonChiffon,
+		"PaleGoldenrod":             PaleGoldenrod,
+		"DarkGoldenrod":             DarkGoldenrod,
+		"Goldenrod":                 Goldenrod,
+		"LightGoldenrodYellow":      LightGoldenrodYellow,
+		"LightYellow":               LightYellow,
+		"PapayaWhip":                PapayaWhip,
+		"Moccasin":                  Moccasin,
+		"PeachPuff":                 PeachPuff,
+		"DarkGreen":                 DarkGreen,
+		"ForestGreen":               ForestGreen,
+		"SeaGreen":                  SeaGreen,
+		"MediumSeaGreen":            MediumSeaGreen,
+		"LimeGreen":                 LimeGreen,
+		"Lime":                      Lime,
+		"SpringGreen":               SpringGreen,
+		"MediumSpringGreen":         MediumSpringGreen,
+		"PaleGreen":                 PaleGreen,
+		"LightGreen":                LightGreen,
+		"DarkSeaGreen":              DarkSeaGreen,
+		"YellowGreen":               YellowGreen,
+		"OliveDrab":                 OliveDrab,
+		"Olive":                     Olive,
+		"DarkOliveGreen":            DarkOliveGreen,
+		"GreenYellow":               GreenYellow,
+		"Chartreuse":                Chartreuse,
+		"LawnGreen":                 LawnGreen,
+		"Teal":                      Teal,
+		"DarkCyan":                  DarkCyan,
+		"LightSeaGreen":             LightSeaGreen,
+		"CadetBlue":                 CadetBlue,
+		"DarkTurquoise":             DarkTurquoise,
+		"MediumTurquoise":           MediumTurquoise,
+		"Turquoise":                 Turquoise,
+		"Aqua":                      Aqua,
+		"Aquamarine":                Aquamarine,
+		"MediumAquamarine":          MediumAquamarine,
+		"PaleTurquoise":             PaleTurquoise,
+		"LightCyan":                 LightCyan,
+		"Navy":                      Navy,
+		"DarkBlue":                  DarkBlue,
+		"MediumBlue":                MediumBlue,
+		"MidnightBlue":              MidnightBlue,
+		"RoyalBlue":                 RoyalBlue,
+		"SteelBlue":                 SteelBlue,
+		"DodgerBlue":                DodgerBlue,
+		"DeepSkyBlue":               DeepSkyBlue,
+		"CornflowerBlue":            CornflowerBlue,
+		"SkyBlue":                   SkyBlue,
+		"LightSkyBlue":              LightSkyBlue,
+		"LightBlue":                 LightBlue,
+		"PowderBlue":                PowderBlue,
+		"LightSteelBlue":            LightSteelBlue,
+		"Indigo":                    Indigo,
+		"Purple":                    Purple,
+		"DarkMagenta":               DarkMagenta,
+		"DarkViolet":                DarkViolet,
+		"DarkSlateBlue":             DarkSlateBlue,
+		"BlueViolet":                BlueViolet,
+		"DarkOrchid":                DarkOrchid,
+		"MediumPurple":              MediumPurple,
+		"MediumOrchid":              MediumOrchid,
+		"Thistle":                   Thistle,
+		"Plum":                      Plum,
+		"Violet":                    Violet,
+		"Orchid":                    Orchid,
+		"MediumSlateBlue":           MediumSlateBlue,
+		"SlateBlue":                 SlateBlue,
+		"RebeccaPurple":             RebeccaPurple,
+		"Snow":                      Snow,
+		"Honeydew":                  Honeydew,
+		"MintCream":                 MintCream,
+		"Azure":                     Azure,
+		"AliceBlue":                 AliceBlue,
+		"GhostWhite":                GhostWhite,
+		"FloralWhite":               FloralWhite,
+		"OldLace":                   OldLace,
+		"Linen":                     Linen,
+		"AntiqueWhite":              AntiqueWhite,
+		"Beige":                     Beige,
+		"Ivory":                     Ivory,
+		"Seashell":                  Seashell,
+		"LavenderBlush":             LavenderBlush,
+		"MistyRose":                 MistyRose,
+		"Lavender":                  Lavender,
+		"MaterialRed500":            MaterialRed500,
+		"MaterialPink500":           MaterialPink500,
+		"MaterialPurple500":         MaterialPurple500,
+		"MaterialDeepPurple500":     MaterialDeepPurple500,
+		"MaterialIndigo500":         MaterialIndigo500,
+		"MaterialBlue500":           MaterialBlue500,
+		"MaterialLightBlue500":      MaterialLightBlue500,
+		"MaterialCyan500":           MaterialCyan500,
+		"MaterialTeal500":           MaterialTeal500,
+		"MaterialGreen500":          MaterialGreen500,
+		"MaterialLightGreen500":     MaterialLightGreen500,
+		"MaterialLime500":           MaterialLime500,
+		"MaterialYellow500":         MaterialYellow500,
+		"MaterialAmber500":          MaterialAmber500,
+		"MaterialOrange500":         MaterialOrange500,
+		"MaterialDeepOrange500":     MaterialDeepOrange500,
+		"MaterialBrown500":          MaterialBrown500,
+		"MaterialGrey500":           MaterialGrey500,
+		"MaterialBlueGrey500":       MaterialBlueGrey500,
+		"FlatTurquoise":             FlatTurquoise,
+		"FlatEmerald":               FlatEmerald,
+		"FlatPeterRiver":            FlatPeterRiver,
+		"FlatAmethyst":              FlatAmethyst,
+		"FlatWetAsphalt":            FlatWetAsphalt,
+		"FlatGreenSea":              FlatGreenSea,
+		"FlatNephritis":             FlatNephritis,
+		"FlatBelizeHole":            FlatBelizeHole,
+		"FlatWisteria":              FlatWisteria,
+		"FlatMidnightBlue":          FlatMidnightBlue,
+		"FlatSunFlower":             FlatSunFlower,
+		"FlatCarrot":                FlatCarrot,
+		"FlatAlizarin":              FlatAlizarin,
+		"FlatClouds":                FlatClouds,
+		"FlatConcrete":              FlatConcrete,
+		"FlatOrange":                FlatOrange,
+		"FlatPumpkin":               FlatPumpkin,
+		"FlatPomegranate":           FlatPomegranate,
+		"FlatSilver":                FlatSilver,
+		"FlatAsbestos":              FlatAsbestos,
+		"FacebookBlue":              FacebookBlue,
+		"TwitterBlue":               TwitterBlue,
+		"LinkedInBlue":              LinkedInBlue,
+		"YouTubeRed":                YouTubeRed,
+		"InstagramPurple":           InstagramPurple,
+		"PinterestRed":              PinterestRed,
+		"TumblrBlue":                TumblrBlue,
+		"SlackPurple":               SlackPurple,
+		"WhatsAppGreen":             WhatsAppGreen,
+		"TelegramBlue":              TelegramBlue,
+		"SnapchatYellow":            SnapchatYellow,
+		"RedditOrange":              RedditOrange,
+		"TwitchPurple":              TwitchPurple,
+		"VimeoBlue":                 VimeoBlue,
+		"Pantone2023VivaMagenta":    Pantone2023VivaMagenta,
+		"Pantone2022VeryPeri":       Pantone2022VeryPeri,
+		"Pantone2021Ultimate":       Pantone2021Ultimate,
+		"Pantone2021Illuminating":   Pantone2021Illuminating,
+		"Pantone2020ClassicBlue":    Pantone2020ClassicBlue,
+		"Pantone2019LivingCoral":    Pantone2019LivingCoral,
+		"Pantone2018UltraViolet":    Pantone2018UltraViolet,
+		"Pantone2017Greenery":       Pantone2017Greenery,
+		"Pantone2016RoseQuartz":     Pantone2016RoseQuartz,
+		"Pantone2016Serenity":       Pantone2016Serenity,
+		"Pantone2015Marsala":        Pantone2015Marsala,
+		"Pantone2014RadiantOrchid":  Pantone2014RadiantOrchid,
+		"Pantone2013Emerald":        Pantone2013Emerald,
+		"Pantone2012TangerineTango": Pantone2012TangerineTango,
+		"Pantone2011Honeysuckle":    Pantone2011Honeysuckle,
+		"Pantone2010Turquoise":      Pantone2010Turquoise,
+		"Pantone2009Mimosa":         Pantone2009Mimosa,
+		"Pantone2008BlueIris":       Pantone2008BlueIris,
+		"Pantone2007ChiliPepper":    Pantone2007ChiliPepper,
+		"Pantone2006SandDollar":     Pantone2006SandDollar,
+		"Pantone2005BlueTurquoise":  Pantone2005BlueTurquoise,
+		"Pantone2004TigerLily":      Pantone2004TigerLily,
+		"Pantone2003Aqua":           Pantone2003Aqua,
+		"Pantone2002TrueRed":        Pantone2002TrueRed,
+		"Pantone2001FuchsiaRose":    Pantone2001FuchsiaRose,
+		"Pantone2000Cerulean":       Pantone2000Cerulean,
+	}
+
+	return colors[name]
+}
+
